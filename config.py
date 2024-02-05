2fff0ff0f975e491fe89fb1adf53420a94d1418c
dimania <dimard@gmail.com>
2024-02-05 17:16:50 +0300

    Start new version

---
+++ config.py
@@ -0,0 +1,21 @@
+#---------------- basic settings --------
+api_id = <API_ID>
+api_hash = <API_HASH> 
+mybot_token = <BOT_TOKEN>
+system_version = "0.2-yorever"
+session_name='nnmbot_session'
+
+channelId = <CHANNEL_ID> # channel what monitor by example       -1001776763737
+chat_my =   <CYANNEL_ID> # channel or chat where send by example -1003333333333
+
+db_name = 'database.db'  # database name, better set full path
+
+# if use proxy set here 
+proxies = {
+  "http": "socks5://127.0.0.1:1080",
+  "https": "socks5://127.0.0.1:1080",
+}
+
+USE_PROXY = 0 # if use proxy set to 1
+
+
