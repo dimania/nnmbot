7f69b983309aba7f45f8187927f4fe476ea5f56e
dimania <dimard@gmail.com>
2025-05-28 00:13:09 +0300

    Need fix switch tag!

--- dbmodule_aio_nnmbot.py
+++ dbmodule_aio_nnmbot.py
@@ -560,6 +560,31 @@ async def main():
         rec_id = await db.db_list_users( id_user, active, rights )
     for row in rec_id: print(f"[db_list_users]={dict(row)}")
 
+   
+
+    async with DatabaseBot(sts.db_name) as db:   
+        rec_id = await db.db_film_by_id(idf=1)
+    for row in rec_id: print(f"[db_film_by_id]={row}")
+    
+    print(f"Add tag: idf:{idf},tag:{sts.SETTAG}, id_user:{id_user}")
+    async with DatabaseBot(sts.db_name) as db:   
+        rec_id = await db.db_add_tag(idf, sts.SETTAG, id_user)
+    for row in rec_id: print(f"[db_add_tag]={row}")
+
+    async with DatabaseBot(sts.db_name) as db:   
+        rec_id = await db.db_switch_user_tag(id_user, sts.UNSETTAG)
+    for row in rec_id: print(f"[db_switch_user_tag]={row}")
+
+    print(f"Add tag: idf:{idf},tag:{sts.SETTAG}, id_user:{id_user}")
+    async with DatabaseBot(sts.db_name) as db:   
+        rec_id = await db.db_add_tag(idf+1, sts.SETTAG, id_user)
+    for row in rec_id: print(f"[db_add_tag]={row}")
+
+    print(f"Add tag: idf:{idf},tag:{sts.SETTAG}, id_user:{id_user}")
+    async with DatabaseBot(sts.db_name) as db:   
+        rec_id = await db.db_add_tag(idf+2, sts.SETTAG, id_user)
+    for row in rec_id: print(f"[db_add_tag]={row}")
+
     async with DatabaseBot(sts.db_name) as db:   
         rec_id = await db.db_list_tagged_films( id_user, tag=sts.SETTAG )
     print(f'(db_list_tagged_films)={rec_id}')
@@ -570,22 +595,11 @@ async def main():
     print(f'(db_list_tagged_films_id)={rec_id}')
     for row in rec_id: print(f"[db_list_tagged_films_id]={dict(row)}")
 
-    async with DatabaseBot(sts.db_name) as db:   
-        rec_id = await db.db_film_by_id(idf=1)
-    for row in rec_id: print(f"[db_film_by_id]={row}")
+   # async with DatabaseBot(sts.db_name) as db:   
+   #     rec_id = await db.db_switch_film_tag(idf, sts.SETTAG, id_user)
+   # for row in rec_id: print(f"[db_switch_film_tag]={row}")
 
-    print(f"Add tag: idf:{idf},tag:{sts.SETTAG}, id_user:{id_user}")
-    async with DatabaseBot(sts.db_name) as db:   
-        rec_id = await db.db_add_tag(idf, sts.SETTAG, id_user)
-    for row in rec_id: print(f"[db_add_tag]={row}")
-
-    async with DatabaseBot(sts.db_name) as db:   
-        rec_id = await db.db_switch_film_tag(idf, sts.SETTAG, id_user)
-    for row in rec_id: print(f"[db_switch_film_tag]={row}")
-
-    async with DatabaseBot(sts.db_name) as db:   
-        rec_id = await db.db_switch_user_tag(id_user, sts.SETTAG)
-    for row in rec_id: print(f"[db_switch_user_tag]={row}")
+    
 
     async with DatabaseBot(sts.db_name) as db:   
         rec_id = await db.db_get_tag( idf, id_user )
