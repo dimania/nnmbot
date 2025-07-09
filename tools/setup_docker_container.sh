23fecc577379a29fb506e5d34e6f4d5daa6cbf21
dimania <dimard@gmail.com>
2025-07-09 21:23:41 +0300

    Correct locale dir

---
+++ tools/setup_docker_container.sh
@@ -0,0 +1,21 @@
+#
+# Create docker container nnmbot 
+#
+
+SRC='../'
+NAME_IMAGE='dimania/nnmbot'
+
+# copy files for container to tmp
+
+temp_dir=$(mktemp -d)
+trap 'rm -rf "$temp_dir"' EXIT
+
+cp ${SRC}/*.py $temp_dir
+cp -R ${SRC}/locales $temp_dir
+cp ${SRC}/requirements.txt $temp_dir
+
+docker build --file ${SRC}/tools/Dockerfile -t ${NAME_IMAGE} $temp_dir
+
+#docker push ${NAME_IMAGE}
+
+rm -rf "$temp_dir"
